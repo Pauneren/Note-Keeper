bf4af3f38b0c049e150aad577e83d42a2f0b72b4
Paula Nerenberg <paulita.glam@gmail.com>
2020-11-24 12:42:32 +0000

    fixed build error

---
+++ build/precache-manifest.da4f3a5c372c9c398259e3a008a6863e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ed8822851532c0f5aafe8719bc2425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b1b339b362e7faec60c",
+    "url": "/static/js/2.b7a02d56.chunk.js"
+  },
+  {
+    "revision": "64c8a124b5a6d08856a4",
+    "url": "/static/js/main.e68603b2.chunk.js"
+  },
+  {
+    "revision": "641f15582db5e9b7b4fe",
+    "url": "/static/js/runtime-main.2f3ca742.js"
+  }
+]);
